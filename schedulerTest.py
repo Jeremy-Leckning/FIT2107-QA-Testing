6e19e5764eb4d8ec5eee2cc69f0c899bf141c39b
Jeremy Leckning <jlec0001@student.monash.edu>
2018-10-18 14:13:20 +1100

    Added more comments

--- schedulerTest.py
+++ schedulerTest.py
@@ -8,10 +8,17 @@ class SchedulerTest(unittest.TestCase):
     """Tests for the scheduler class.  Add more tests
     to test the code that you write"""
     def setUp(self):
+        """
+        the setUp method is run before the start of every test_ method
+        """
         self.scheduler = Scheduler()
 
     def test_max(self):
-        """Tests functionality of max() function using mocking"""
+        """
+        Tests functionality of max() function using mocking
+        return data values of load_satellites() and satellite_visibility function were mocked
+        @raises: AssertionError if any of the tests fails
+        """
         realScheduler = Scheduler()
         realScheduler.load_satellites= MagicMock(return_value={0: "sat1", 1:"sat2", 2:"sat3"})
         realScheduler.satellite_visibility = MagicMock()
@@ -25,7 +32,11 @@ class SchedulerTest(unittest.TestCase):
 
 
     def test_total(self):
-        """Tests functionality of total() function using mocking"""
+        """
+        Tests functionality of total() function using mocking
+        return data values of load_satellites() and satellite_visibility function were mocked
+        @raises: AssertionError if any of the tests fails
+        """
         realScheduler = Scheduler()
         realScheduler.load_satellites = MagicMock(return_value={0: "sat1", 1: "sat2", 2: "sat3", 3:"sat4", 4:"sat5"})
         realScheduler.satellite_visibility = MagicMock()
@@ -38,7 +49,11 @@ class SchedulerTest(unittest.TestCase):
         self.assertTrue(current_max_list == ["sat1", "sat3", "sat4"])
 
     def test_findTime_cumulative_false(self):
-        """Tests functionality of find_time when cumulative = False, using mocking"""
+        """
+        Tests functionality of find_time when cumulative = False, using mocking
+        the return data value of max() method was mocked to test findtime method
+        @raises: AssertionError if any of the tests fails
+        """
         realScheduler = Scheduler()
         realScheduler.max = MagicMock()
         realScheduler.max.side_effect = [("00:00", 4, ["sat1", "sat2", "sat3", "sat4"]),("00:00", 2, ["sat1", "sat2"]),
@@ -51,7 +66,11 @@ start_time=datetime.now(), n_windows=3, duration=60, sample_interval=1, cumulati
         self.assertTrue(realScheduler.max.call_count == 3)
 
     def test_findTime_cumulative_true(self):
-        """Tests functionality of find_time when cumulative = True, using mocking"""
+        """
+        Tests functionality of find_time when cumulative = True, using mocking
+        the return data value of max() method was mocked to test findtime method
+        @raises: AssertionError if any of the tests fails
+        """
         realScheduler = Scheduler()
         realScheduler.total = MagicMock()
         realScheduler.total.side_effect = [("00:00", 4, ["sat1", "sat6", "sat3", "sat4"]), ("01:00", 2, ["sat1", "sat7"]),
@@ -69,6 +88,8 @@ start_time=datetime.now(), n_windows=3, duration=60, sample_interval=1, cumulati
     def test_load_satellites(self):
         """
         tests load satellites using Mocking
+        testing that load_satellites() function is being called correctly
+        @raises: AssertionError if any of the tests fails
         """
         schedulerMock = create_autospec(self.scheduler)
         schedulerMock.load_satellites.return_value = ["sat1", "sat2", "sat3", "sat4"]
@@ -79,7 +100,10 @@ start_time=datetime.now(), n_windows=3, duration=60, sample_interval=1, cumulati
 
     def test_exceptionthrown(self):
         """
-        tests whether IllegalArguentExceptions are thrown when an illegal argument is used in the find_time function
+        tests whether IllegalArgumentExceptions are thrown when an illegal argument is used in the find_time function
+        these tests are used to check whether an IllegalArgumentException is thrown every time an invalid argument
+        is used for the findtime method.
+        @raises: AssertionError if any of the tests fails
         """
         with self.assertRaises(IllegalArgumentException):
             (stime, satellites) = self.scheduler.find_time(start_time="now")
@@ -99,6 +123,7 @@ start_time=datetime.now(), n_windows=3, duration=60, sample_interval=1, cumulati
             (stime, satellites) = self.scheduler.find_time(location=(10,200))
         with self.assertRaises(IllegalArgumentException):
             (stime, satellites) = self.scheduler.find_time(cumulative="hello")
+
 
 if __name__ == "__main__":
     unittest.main()
